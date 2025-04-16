b46e2c87e0fd7db0f29332a96ac000aa236b986d
randy <randyn1080@gmail.com>
2025-04-16 10:51:01 -0400

    removed javadocs until complete

--- src/main/java/com/randyn1080/pepprojectpractice/dao/AccountDAO.java
+++ src/main/java/com/randyn1080/pepprojectpractice/dao/AccountDAO.java
@@ -3,17 +3,6 @@ package com.randyn1080.pepprojectpractice.dao;
 import com.randyn1080.pepprojectpractice.model.Account;
 
 public interface AccountDAO {
-    /**
-     * Create account
-     * @param account
-     * @return
-     */
     Account createAccount(Account account);
-
-    /**
-     * Read account
-     * @param accountId
-     * @return
-     */
     Account getAccountById(int accountId);
 }
--- src/main/java/com/randyn1080/pepprojectpractice/dao/AccountDAOImpl.java
+++ src/main/java/com/randyn1080/pepprojectpractice/dao/AccountDAOImpl.java
@@ -3,23 +3,12 @@ package com.randyn1080.pepprojectpractice.dao;
 import com.randyn1080.pepprojectpractice.model.Account;
 
 public class AccountDAOImpl implements AccountDAO {
-    /**
-     * Create account
-     *
-     * @param account
-     * @return
-     */
     @Override
     public Account createAccount(Account account) {
         return null;
     }
 
-    /**
-     * Read account
-     *
-     * @param accountId
-     * @return
-     */
+
     @Override
     public Account getAccountById(int accountId) {
         return null;
