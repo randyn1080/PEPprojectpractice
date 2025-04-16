25a25b67f344909abfacd96bfc13da1265d05336
randy <randyn1080@gmail.com>
2025-04-16 11:30:14 -0400

    removed javadocs until complete

--- src/main/java/com/randyn1080/pepprojectpractice/dao/AccountDAOImpl.java
+++ src/main/java/com/randyn1080/pepprojectpractice/dao/AccountDAOImpl.java
@@ -1,11 +1,61 @@
 package com.randyn1080.pepprojectpractice.dao;
 
 import com.randyn1080.pepprojectpractice.model.Account;
+import com.randyn1080.pepprojectpractice.util.ConnectionUtil;
+
+import java.sql.*;
 
 public class AccountDAOImpl implements AccountDAO {
     @Override
     public Account createAccount(Account account) {
-        String sql = "INSERT INTO account (account_id, username, password) VALUES (?,?,?)";
+        String sql = "INSERT INTO account (username, password) VALUES (?,?)";
+
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            connection = ConnectionUtil.getConnection();
+            pstmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, account.getUsername());
+            pstmt.setString(2, account.getPassword());
+
+            pstmt.executeUpdate();
+
+            rs = pstmt.getGeneratedKeys();
+            if (rs.next()) {
+                int generatedAccountId = rs.getInt(1);
+                account.setAccount_id(generatedAccountId);
+                return account;
+            }
+
+        } catch (SQLException e) {
+            //TODO: handle exception
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    //TODO: handle exception
+                }
+            }
+            if (pstmt != null){
+                try {
+                    pstmt.close();
+                } catch(SQLException e) {
+                    //TODO: handle exception
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    //TODO: handle exception
+                }
+            }
+        }
+
+        return null;
     }
 
 
