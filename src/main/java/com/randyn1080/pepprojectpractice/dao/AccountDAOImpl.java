a96c1c8302e497b552905d2ef98e538abb61cf44
randy <randyn1080@gmail.com>
2025-04-16 11:04:55 -0400

    removed javadocs until complete

--- src/main/java/com/randyn1080/pepprojectpractice/dao/AccountDAOImpl.java
+++ src/main/java/com/randyn1080/pepprojectpractice/dao/AccountDAOImpl.java
@@ -5,7 +5,7 @@ import com.randyn1080.pepprojectpractice.model.Account;
 public class AccountDAOImpl implements AccountDAO {
     @Override
     public Account createAccount(Account account) {
-        return null;
+        String sql = "INSERT INTO account (account_id, username, password) VALUES (?,?,?)";
     }
 
 
