2170080c2aa30eea870da5e0297f92f4c7dd7aaf
randy <randyn1080@gmail.com>
2025-04-18 12:26:46 -0400

    Implemented error handling on methods without it, and complete JavaDocs

--- src/main/java/com/randyn1080/pepprojectpractice/controller/SocialMediaController.java
+++ src/main/java/com/randyn1080/pepprojectpractice/controller/SocialMediaController.java
@@ -13,9 +13,12 @@ import java.util.List;
 import java.util.Objects;
 
 /**
- * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
- * found in readme.md as well as the test cases. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
+ * Controller class to handle social media functionalities, including
+ * user account management (register, login) and message management
+ * (creation, retrieval, update, and deletion).
+ *
+ * This class uses Javalin to define and configure the API endpoints and
+ * interacts with the underlying services for business logic execution.
  */
 public class SocialMediaController {
     /**
